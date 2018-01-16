b3b1410446a7083d38389883d0f66200153b4b6e
mickey0524 <buptbh@163.com>
2018-01-16 23:52:36 +0800

    feat(prompt): 增加提示框组件

---
+++ TTgame/components/common/Prompt.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  Modal,
+  Dimensions,
+  PixelRatio,
+  StyleSheet,
+} from 'react-native';
+
+const { width: totalWidth, height: totalHeight } = Dimensions.get('window');
+const dpr = PixelRatio.get();
+
+export default class Prompt extends Component {
+  constructor(props) {
+    super(props);
+    this.onClickCancel = this.onClickCancel.bind(this);
+    this.onClickDetermine = this.onClickDetermine.bind(this);
+  }
+
+  render() {
+    return (
+      <View>
+        <Modal
+          visible={this.props.visible}
+          animationType={'fade'}
+          transparent={true} >
+          <View style={styles.container}>
+            <View style={styles.promptWrap}>
+              <View style={styles.titleWrap}>
+                <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{this.props.title}</Text>
+                <Text style={{ marginTop: 2 }}>{this.props.content}</Text>
+              </View>
+              <View style={styles.bottomBtnWrap}>
+                {
+                  this.props.isAskModal &&
+                  <View style={[styles.bottomBtn, { borderRightWidth: 1 / dpr, borderStyle: 'solid', borderColor: '#E8E8E8' }]}>
+                    <Text style={styles.btnText} onPress={this.onClickCancel}>取消</Text>
+                  </View>
+                }
+                <View style={styles.bottomBtn}>
+                  <Text style={styles.btnText} onPress={this.onClickDetermine}>确定</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    )
+  }
+
+  onClickCancel() {
+    (this.props.onClickCancel && this.props.onClickCancel());
+  }
+
+  onClickDetermine() {
+    (this.props.onClickDetermine && this.props.onClickDetermine());
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, .4)',
+  },
+  promptWrap: {
+    backgroundColor: '#FFF',
+    width: totalWidth * 0.75,
+    borderRadius: 10,
+    overflow: 'hidden', 
+  },
+  titleWrap: {
+    padding: 15,
+    display: 'flex',
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderColor: '#E8E8E8',
+    borderBottomWidth: 1 / dpr,
+  },
+  bottomBtnWrap: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  bottomBtn: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 15,
+  },
+  btnText: {
+    fontSize: 16,
+    color: '#4ba7ee',
+  },
+});
--- TTgame/components/pagelet/Drawer/Setting.js
+++ TTgame/components/pagelet/Drawer/Setting.js
@@ -5,9 +5,11 @@ import {
   Text,
   Switch,
   AsyncStorage,
+  TouchableWithoutFeedback,
   StyleSheet,
 } from 'react-native';
 
+import Prompt from '../../common/Prompt';
 import ToolBar from '../../common/ToolBar';
 import { MyStatusBar } from '../../common/MyStatusBar';
 import { changeMode } from '../../../redux/action/mode';
@@ -21,14 +23,23 @@ class Setting extends Component {
     this.state = {
       isNightMode: this.props.mode == 'night',
       isMobileNetAutoLoadImg: this.props.loadImgWithoutWifi,
+      modalVisible: false,
     }
     this.onSwitchChange = this.onSwitchChange.bind(this);
+    this.onPressClearStorage = this.onPressClearStorage.bind(this);
   }
   
   render() {
     let isNightMode = this.state.isNightMode;
     return (
       <View style={{ flex: 1, backgroundColor: isNightMode ? '#252525' : '#FFF' }}>
+        <Prompt 
+          title={'清除缓存'}
+          content={'确定要清除缓存的内容吗?'}
+          isAskModal={true}
+          onClickCancel={() => { this.setState({ modalVisible: false }); }}
+          onClickDetermine={() => { this.setState({ modalVisible: false }); clearHistory(); }}
+          visible={this.state.modalVisible} />
         <MyStatusBar />
         <ToolBar title={'设置'} navigation={this.props.navigation} leftIcon={'back'} />
         <View>
@@ -44,12 +55,13 @@ class Setting extends Component {
               value={this.state.isMobileNetAutoLoadImg} 
               onValueChange={(newVal) => this.onSwitchChange(newVal, 'isMobileNetAutoLoadImg')}/>        
           </View>
-          <View style={styles.optionWrap}>
-            <Text style={[styles.optionTitle, { color: isNightMode ? '#FFF' : '#000' }]} 
-              onPress={this.onPressClearStorage}>
-              清空缓存
-            </Text>
-          </View>
+          <TouchableWithoutFeedback onPress={this.onPressClearStorage}>
+            <View style={styles.optionWrap}>
+              <Text style={[styles.optionTitle, { color: isNightMode ? '#FFF' : '#000' }]}>
+                清空缓存
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
         </View>
       </View>
     );
@@ -78,7 +90,9 @@ class Setting extends Component {
    * 点击清空缓存
    */
   onPressClearStorage() {
-    clearHistory();
+    this.setState({
+      modalVisible: true,
+    });
   }
 }
 
